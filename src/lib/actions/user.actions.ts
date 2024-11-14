c2473b9381c4e9909bdde89199ff6abcbea260a5
RonanKer29 <kervella.ronan@hotmail.com>
2024-11-14 11:28:13 +0100

    otp working

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,5 +1,3 @@
-"use server";
-
 //*Create account flow :
 //*1. User enters full name and email
 //*2. Check if the user already exists using the email (we will use this to identify if we still need to create a user document or not)
@@ -9,7 +7,43 @@
 //*6. Return the user's accountId that will be used to complete the login
 //*7. verify the OTP and authenticate to login
 
-const createAccount = async ({
+"use server";
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { Query, ID } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
   fullName,
   email,
 }: {
@@ -17,4 +51,26 @@ const createAccount = async ({
   email: string;
 }) => {
   const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify({ accountId });
 };
