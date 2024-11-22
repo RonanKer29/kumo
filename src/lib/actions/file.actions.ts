f3a9a48dd6ec6d801a8600b3a06460aa6404c661
RonanKer29 <kervella.ronan@hotmail.com>
2024-11-22 10:28:34 +0100

    uploads working for all types

--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -29,6 +29,7 @@ export const uploadFile = async ({
       ID.unique(),
       inputFile
     );
+
     const fileDocument = {
       type: getFileType(bucketFile.name).type,
       name: bucketFile.name,
@@ -132,6 +133,28 @@ export const updateFileUsers = async ({
     );
     revalidatePath(path);
     return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Failed to rename file");
+  }
+};
+
+export const deleteFile = async ({
+  fileId,
+  bucketFileId,
+  path,
+}: DeleteFileProps) => {
+  const { databases, storage } = await createAdminClient();
+  try {
+    const deletedFile = await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId
+    );
+    if (deletedFile) {
+      await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
+    }
+    revalidatePath(path);
+    return parseStringify({ status: "success" });
   } catch (error) {
     handleError(error, "Failed to rename file");
   }
