7479187524fd1f816e19d440a9fb2ef88522411e
RonanKer29 <kervella.ronan@hotmail.com>
2024-11-27 13:55:42 +0100

    dashboard ok

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import Link from "next/link";
 import { Models } from "node-appwrite";
-import { ActionDropdown } from "@/components/ActionDropdown";
+import ActionDropDown from "@/components/ActionDropdown";
 import { Chart } from "@/components/Chart";
 import { FormattedDateTime } from "@/components/FormattedDateTime";
 import { Thumbnail } from "@/components/Thumbnail";
@@ -63,8 +63,38 @@ const Dashboard = async () => {
       </section>
 
       <section className="dashboard-recent-files">
-        {/* Recent files uploaded */}
-        <div className="">Recent files uploaded</div>
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <ActionDropDown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
       </section>
     </div>
   );
