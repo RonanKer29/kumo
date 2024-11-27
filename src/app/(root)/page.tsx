d20c6f267259fc4815a5212f577dab78db547e68
RonanKer29 <kervella.ronan@hotmail.com>
2024-11-27 10:59:19 +0100

    dashboard with file usage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import Image from "next/image";
 
-const SignIn = () => {
-  return <div>Page coucou</div>;
+import Link from "next/link";
+import { Models } from "node-appwrite";
+import { ActionDropdown } from "@/components/ActionDropdown";
+import { Chart } from "@/components/Chart";
+import { FormattedDateTime } from "@/components/FormattedDateTime";
+import { Thumbnail } from "@/components/Thumbnail";
+import { Separator } from "@/components/ui/separator";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
+
+const Dashboard = async () => {
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+
+    getTotalSpaceUsed(),
+  ]);
+
+  const usageSummary = getUsageSummary(totalSpace);
+
+  return (
+    <div className="dashboard-container">
+      <section>
+        <Chart used={totalSpace.used} />
+
+        {/* Uploaded file type summaries */}
+
+        <ul className="dashboard-summary-list">
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className="dashboard-summary-card"
+            >
+              <div className="space-y-4">
+                <div className="flex justify-between gap-3">
+                  <Image
+                    src={summary.icon}
+                    width={100}
+                    height={100}
+                    alt="uploaded image"
+                    className="summary-type-icon"
+                  />
+
+                  <h4 className="summary-type-size">
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+
+                <h5 className="summary-type-title">{summary.title}</h5>
+
+                <Separator className="bg-light-400" />
+
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className="text-center"
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="dashboard-recent-files">
+        {/* Recent files uploaded */}
+        <div className="">Recent files uploaded</div>
+      </section>
+    </div>
+  );
 };
 
-export default SignIn;
+export default Dashboard;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,29 @@ export function cn(...inputs: ClassValue[]) {
 export const parseStringify = (value: unknown) =>
   JSON.parse(JSON.stringify(value));
 
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export const convertFileSize = (sizeInBytes: number, digits?: number) => {
+  if (sizeInBytes < 1024) {
+    return sizeInBytes + " Bytes"; // Less than 1 KB, show in Bytes
+  } else if (sizeInBytes < 1024 * 1024) {
+    const sizeInKB = sizeInBytes / 1024;
+    return sizeInKB.toFixed(digits || 1) + " KB"; // Less than 1 MB, show in KB
+  } else if (sizeInBytes < 1024 * 1024 * 1024) {
+    const sizeInMB = sizeInBytes / (1024 * 1024);
+    return sizeInMB.toFixed(digits || 1) + " MB"; // Less than 1 GB, show in MB
+  } else {
+    const sizeInGB = sizeInBytes / (1024 * 1024 * 1024);
+    return sizeInGB.toFixed(digits || 1) + " GB"; // 1 GB or more, show in GB
+  }
+};
+
+export const calculatePercentage = (sizeInBytes: number) => {
+  const totalSizeInBytes = 2 * 1024 * 1024 * 1024; // 2GB in bytes
+  const percentage = (sizeInBytes / totalSizeInBytes) * 100;
+  return Number(percentage.toFixed(2));
+};
+
 export const getFileType = (fileName: string) => {
   const extension = fileName.split(".").pop()?.toLowerCase();
 
@@ -52,9 +75,40 @@ export const getFileType = (fileName: string) => {
   return { type: "other", extension };
 };
 
-export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+export const formatDateTime = (isoString: string | null | undefined) => {
+  if (!isoString) return "—";
 
-type FileType = "document" | "image" | "video" | "audio" | "other";
+  const date = new Date(isoString);
+
+  // Get hours and adjust for 12-hour format
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+  const period = hours >= 12 ? "pm" : "am";
+
+  // Convert hours to 12-hour format
+  hours = hours % 12 || 12;
+
+  // Format the time and date parts
+  const time = `${hours}:${minutes.toString().padStart(2, "0")}${period}`;
+  const day = date.getDate();
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[date.getMonth()];
+
+  return `${time}, ${day} ${month}`;
+};
 
 export const getFileIcon = (
   extension: string | undefined,
@@ -118,6 +172,61 @@ export const getFileIcon = (
   }
 };
 
+// APPWRITE URL UTILS
+// Construct appwrite file URL - https://appwrite.io/docs/apis/rest#images
+export const constructFileUrl = (bucketFileId: string) => {
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+};
+
+export const constructDownloadUrl = (bucketFileId: string) => {
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+};
+
+// DASHBOARD UTILS
+interface TotalSpace {
+  document: { size: number; latestDate: string };
+  image: { size: number; latestDate: string };
+  video: { size: number; latestDate: string };
+  audio: { size: number; latestDate: string };
+  other: { size: number; latestDate: string };
+}
+
+export const getUsageSummary = (totalSpace: TotalSpace) => {
+  return [
+    {
+      title: "Documents",
+      size: totalSpace.document.size,
+      latestDate: totalSpace.document.latestDate,
+      icon: "/assets/icons/file-document-light.svg",
+      url: "/documents",
+    },
+    {
+      title: "Images",
+      size: totalSpace.image.size,
+      latestDate: totalSpace.image.latestDate,
+      icon: "/assets/icons/file-image-light.svg",
+      url: "/images",
+    },
+    {
+      title: "Media",
+      size: totalSpace.video.size + totalSpace.audio.size,
+      latestDate:
+        totalSpace.video.latestDate > totalSpace.audio.latestDate
+          ? totalSpace.video.latestDate
+          : totalSpace.audio.latestDate,
+      icon: "/assets/icons/file-video-light.svg",
+      url: "/media",
+    },
+    {
+      title: "Others",
+      size: totalSpace.other.size,
+      latestDate: totalSpace.other.latestDate,
+      icon: "/assets/icons/file-other-light.svg",
+      url: "/others",
+    },
+  ];
+};
+
 export const getFileTypesParams = (type: string) => {
   switch (type) {
     case "documents":
@@ -133,62 +242,8 @@ export const getFileTypesParams = (type: string) => {
   }
 };
 
-// APPWRITE URL UTILS
-// Construct appwrite file URL - https://appwrite.io/docs/apis/rest#images
-export const constructFileUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
-};
-
-export const convertFileSize = (sizeInBytes: number, digits?: number) => {
-  if (sizeInBytes < 1024) {
-    return sizeInBytes + " Bytes"; // Less than 1 KB, show in Bytes
-  } else if (sizeInBytes < 1024 * 1024) {
-    const sizeInKB = sizeInBytes / 1024;
-    return sizeInKB.toFixed(digits || 1) + " KB"; // Less than 1 MB, show in KB
-  } else if (sizeInBytes < 1024 * 1024 * 1024) {
-    const sizeInMB = sizeInBytes / (1024 * 1024);
-    return sizeInMB.toFixed(digits || 1) + " MB"; // Less than 1 GB, show in MB
-  } else {
-    const sizeInGB = sizeInBytes / (1024 * 1024 * 1024);
-    return sizeInGB.toFixed(digits || 1) + " GB"; // 1 GB or more, show in GB
-  }
-};
-
-export const formatDateTime = (isoString: string | null | undefined) => {
-  if (!isoString) return "—";
-
-  const date = new Date(isoString);
-
-  // Get hours and adjust for 12-hour format
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
-  const period = hours >= 12 ? "pm" : "am";
-
-  // Convert hours to 12-hour format
-  hours = hours % 12 || 12;
-
-  // Format the time and date parts
-  const time = `${hours}:${minutes.toString().padStart(2, "0")}${period}`;
-  const day = date.getDate();
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthNames[date.getMonth()];
-
-  return `${time}, ${day} ${month}`;
-};
-
-export const constructDownloadUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+export const calculateAngle = (sizeInBytes: number) => {
+  const totalSizeInBytes = 2 * 1024 * 1024 * 1024; // 2GB in bytes
+  const percentage = (sizeInBytes / totalSizeInBytes) * 360;
+  return Number(percentage.toFixed(2));
 };
